09ee0c4148b335852dab3f4bfe05a3137a6d8909
YANARICO <luchin.ly50@gmail.com>
2024-06-28 17:28:25 -0400

    edit landing page text

--- src/pages/Reportes.jsx
+++ src/pages/Reportes.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
 import { db } from '../data/FireBase';
 import { Link } from "react-router-dom";
+import swal from 'sweetalert';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const Reportes = () => {
   const [reportes, setReportes] = useState([]);
@@ -19,13 +22,29 @@ const Reportes = () => {
     fetchReportes();
   }, []);
 
+  const handleDelete = async (id) => {
+    const result = await swal({
+      title: '¿Estás seguro que desea eliminar?',
+      text: "¡No podrás revertir esto!",
+      icon: 'warning',
+      buttons: true,
+      dangerMode: true,
+    });
+
+    if (result) {
+      await deleteDoc(doc(db, "Reportes", id));
+      setReportes(reportes.filter(reporte => reporte.id !== id));
+      swal('¡Eliminado!', 'El reporte ha sido eliminado.', 'success');
+    }
+  };
+
   return (
     <div className="container mx-auto p-4">
       <h2 className="text-2xl font-semibold text-gray-800 dark:text-white mb-4">Lista de Reportes</h2>
       <table className="table-auto w-full border-collapse border border-gray-200 bg-white">
         <thead>
           <tr className="bg-blue-400 text-white">
-           <th className="px-4 py-2">Nombre</th>
+            <th className="px-4 py-2">Nombre</th>
             <th className="px-4 py-2">Línea Seleccionada</th>
             <th className="px-4 py-2">Área del Terreno</th>
             <th className="px-4 py-2">Zona Ecológica</th>
@@ -35,14 +54,22 @@ const Reportes = () => {
         <tbody>
           {reportes.map((reporte) => (
             <tr key={reporte.id} className="border-t">
-                 <td className="border px-4 py-2">{reporte.nombreSeleccion}</td>
+              <td className="border px-4 py-2">{reporte.nombreSeleccion}</td>
               <td className="border px-4 py-2">{reporte.LineaSeleccionada}</td>
               <td className="border px-4 py-2">{reporte.area_terreno}</td>
               <td className="border px-4 py-2">{reporte.zonaEcologica}</td>
-              <td className="border px-4 py-2 text-center">
-                <Link to={`/dashboard/detalle-reporte/${reporte.id}`} className="bg-blue-500 text-white px-2 py-1 rounded">
-                  Detalles
+              <td className="border px-4 py-2 text-center space-x-2">
+                <Link to={`/dashboard/detalle-reporte/${reporte.id}`} className="bg-blue-500 text-white px-2 py-1 rounded inline-flex items-center">
+                  <FontAwesomeIcon icon={faEye} />
+                  
                 </Link>
+                <button
+                  onClick={() => handleDelete(reporte.id)}
+                  className="bg-red-500 text-white px-2 py-1 rounded inline-flex items-center"
+                >
+                  <FontAwesomeIcon icon={faTrash}/>
+                  
+                </button>
               </td>
             </tr>
           ))}
